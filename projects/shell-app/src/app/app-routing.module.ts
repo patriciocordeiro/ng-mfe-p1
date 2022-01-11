19bb29e7a3c67ca4412e7c1cfa9826e50c2919df
patriciocordeiro <patricio.cordeiro@institutosoma.org.br>
2022-01-11 20:59:07 -0300

    first commit

---
+++ projects/shell-app/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('../../../../dist/home-app-mfe-lib').then(m => m.HomeModule)
+
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('../../../../dist/user-app-mfe-lib').then(m => m.UserModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
